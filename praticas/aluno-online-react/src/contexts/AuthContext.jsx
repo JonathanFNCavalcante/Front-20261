e9cd622949eb624b225961dec10e7baef4c31067
Jonathan Cavalcante <jonathan.francisco@iesb.edu.br>
2026-05-18 09:08:37 -0300

    fix: separa AuthContext e AuthProvider em arquivos distintos para o ESLint

--- praticas/aluno-online-react/src/contexts/AuthContext.jsx
+++ praticas/aluno-online-react/src/contexts/AuthContext.jsx
@@ -1,28 +1,3 @@
-import { createContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { createContext } from 'react';
 
 export const AuthContext = createContext();
-
-export const AuthProvider = ({ children }) => {
-  const [autenticado, setAutenticado] = useState(false);
-  const [usuario, setUsuario] = useState(null);
-  const navigate = useNavigate();
-
-  const login = (dadosUsuario) => {
-    setAutenticado(true);
-    setUsuario({ nome: dadosUsuario.email.split('@')[0], email: dadosUsuario.email });
-    navigate('/');
-  };
-
-  const logout = () => {
-    setAutenticado(false);
-    setUsuario(null);
-    navigate('/login');
-  };
-
-  return (
-    <AuthContext.Provider value={{ autenticado, usuario, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
---
+++ praticas/aluno-online-react/src/contexts/AuthProvider.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from './AuthContext';
+
+export const AuthProvider = ({ children }) => {
+  const [autenticado, setAutenticado] = useState(false);
+  const [usuario, setUsuario] = useState(null);
+  const navigate = useNavigate();
+
+  const login = (dadosUsuario) => {
+    setAutenticado(true);
+    setUsuario({ nome: dadosUsuario.email.split('@')[0], email: dadosUsuario.email });
+    navigate('/');
+  };
+
+  const logout = () => {
+    setAutenticado(false);
+    setUsuario(null);
+    navigate('/login');
+  };
+
+  return (
+    <AuthContext.Provider value={{ autenticado, usuario, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
